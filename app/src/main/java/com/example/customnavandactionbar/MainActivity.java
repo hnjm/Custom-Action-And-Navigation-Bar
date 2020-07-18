116444e21f29a1fee17ddc2544ce37d5d69ceb2e
mahmud.jerry <mahmud.jerry.me@gmail.com>
2020-07-18 20:23:04 +0600

    Navigation Activity Created

--- app/src/main/java/com/example/customnavandactionbar/MainActivity.java
+++ app/src/main/java/com/example/customnavandactionbar/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import static java.lang.Thread.sleep;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start();
+
     }
 }
